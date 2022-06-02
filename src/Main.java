3e9c5948f92c8d566696b18a574b266c55be83dc
Parauan <parauaan@gmail.com>
2022-06-02 20:53:29 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello \n" +
+                "Vasile");
+
+
+        int x = 2;
+        double y = 3;
+        double z = x + y;
+        System.out.println(z);
+
+        System.out.println(50/3);
+
+        System.out.println(-5 + 8 * 6);
+
+        System.out.println((55+9) % 9 );
+
+        System.out.println(20 + -3*5 / 8 );
+
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+    }
+}
